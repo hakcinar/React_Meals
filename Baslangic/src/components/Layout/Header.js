105480c6ad214e9e0c4629f94820bb8639a8cd26
Hakan Akcinar <hakcinar1@gmail.com>
2022-09-24 22:57:58 +0300

    Finished

---
+++ Baslangic/src/components/Layout/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import mealsImage from '../../assets/meals.jpg';
+import classes from './Header.module.css'
+import HeaderCartButton from './HeaderCartButton';
+const Header = (props) => {
+  return (
+    <>
+    <header className={classes.header}>
+        <h1>React Meals</h1>
+        <HeaderCartButton onClick={props.onShowCart}>Cart</HeaderCartButton>
+    </header>
+    <div className={classes['main-image']}>
+        <img src={mealsImage} alt="table of foods"></img>
+    </div>
+    </>
+  )
+}
+
+export default Header
